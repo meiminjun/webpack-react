256c8ad23650a04ad0381dd1ee936a34beb15a3a
meiminjun <251222845@qq.com>
2016-07-17 11:57:08 +0800

    删除多余文件

--- app/index.js
+++ app/index.js
@@ -8,17 +8,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './demo/demo.scss';
-
+import './main.scss';
 import ListDemo from './component/ListDemo';
-
 
 const data = [
     {name:"张文娟",text: "换新设计，内外兼修才够美~~穿出独属于你自己的腔调"},
     {name:"李梅",text: "内衣，情趣，呵呵呵呵呵"},
     {name:"王月月",text: "内在美才是真的美"}
   ];
-
 
 ReactDOM.render( < ListDemo  data = {data}/ > ,
 	document.getElementById('root')
